d4ca848d5cf6c7f3fdd61c0498c9a0b6d8ed2d70
zsjsll <124119885@qq.com>
2026-08-10 01:01:58 +0800

    build: 准备构建脚本中的路径处理
    引入 PathBuf 类型以增强路径操作。
    定义 source_dir 变量，指向 bin\ 目录，为后续文件操作做准备。
    调整 target_dir 的引用方式。

--- build.rs
+++ build.rs
@@ -1,11 +1,18 @@
-use std::{env, path::Path};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
 
 fn main() {
     let profile = env::var("PROFILE").unwrap(); // debug 或 release
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let target_dir = Path::new(&manifest_dir).join("target").join(&profile);
+    let target_dir = &Path::new(&manifest_dir).join("target").join(&profile);
 
     println!("cargo:warning=🔥 11111111111111111111111111111111");
-    println!("cargo:warning={}",&target_dir.display());
+    println!("cargo:warning={}", target_dir.display());
     // panic!("BUILD_RS_IS_RUNNING_PANIC");
+
+    let source_dir = Path::new(r"bin\");
+
+
 }
