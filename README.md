b28d37a8c51c0ce178c2cece2b4bf89fb8060477
zsjsll <124119885@qq.com>
2026-08-11 16:54:45 +0800

    feat: 改进构建脚本并更新依赖
    改进构建脚本，使其在构建时自动复制必要文件到输出目录，并更新 `README.md` 中的使用说明。

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # windows_auto_backup
-调用 snapshot64.exe, 把系统备份到支持 smb 协议的 NAS 中
+
+调用 snapshot64.exe 自动备份系统
+
+## 使用方式
+
+1. 把 `config copy.toml` 改名为 `config.toml`
+2. cmd 中运行 `deploy.cmd` 编译到 deploy 文件夹下
+
+具体参数查看 `config copy.toml`
--- build.rs
+++ build.rs
@@ -1,18 +1,36 @@
-use std::{
-    env,
-    path::{Path, PathBuf},
-};
+// use std::{env, fs, io, path::Path};
 
-fn main() {
-    let profile = env::var("PROFILE").unwrap(); // debug 或 release
-    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let target_dir = &Path::new(&manifest_dir).join("target").join(&profile);
+// fn cp_dir(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+//     fs::create_dir_all(&dst)?;
 
-    println!("cargo:warning=🔥 11111111111111111111111111111111");
-    println!("cargo:warning={}", target_dir.display());
-    // panic!("BUILD_RS_IS_RUNNING_PANIC");
+//     for entry in fs::read_dir(&src)? {
+//         let entry = entry?;
+//         let src_path = entry.path();
+//         let dst_path = dst.as_ref().join(entry.file_name());
 
-    let source_dir = Path::new(r"bin\");
+//         if entry.file_type()?.is_file() {
+//             fs::copy(src_path, dst_path)?;
+//         } else {
+//             cp_dir(src_path, dst_path)?
+//         }
+//     }
 
+//     Ok(())
+// }
 
-}
+// fn main() {
+//     let source_dir = Path::new(r"bin\");
+
+//     let profile = env::var("PROFILE").unwrap(); // debug 或 release
+//     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+//     let target_dir = &Path::new(&manifest_dir)
+//         .join("target")
+//         .join(&profile)
+//         .join(source_dir);
+
+//     println!("cargo:warning=🔥");
+
+//     cp_dir(source_dir, target_dir).unwrap();
+// }
+
+fn main() {}
